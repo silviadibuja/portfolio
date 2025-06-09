60b318361d7cca429eff23801f9f1f99861c3d9f
eskalona17 <fer.escalonso@gmail.com>
2025-06-09 16:34:13 +0200

    fix deploy

---
+++ src/data/ilustraciones.js
@@ -0,0 +1,28 @@
+// src/data/ilustraciones.js
+export const ilustraciones = [
+  {
+    src: "https://images.unsplash.com/vector-1749272151392-0189e836c426?q=80&w=2360&auto=format&fit=crop",
+    alt: "Ilustración abstracta con formas geométricas",
+    span: "span-2x2"
+  },
+  {
+    src: "https://plus.unsplash.com/premium_vector-1739921187869-0562a09fe3f9?q=80&w=2030&auto=format&fit=crop",
+    alt: "Ilustración vectorial de paisaje",
+    span: "span-1x1"
+  },
+  {
+    src: "https://images.unsplash.com/photo-1522199710521-72d69614c702?q=80&w=2400&auto=format&fit=crop",
+    alt: "Escena artística en tonos cálidos",
+    span: "span-3x1"
+  },
+  {
+    src: "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?q=80&w=2400&auto=format&fit=crop",
+    alt: "Ilustración de retrato digital",
+    span: "span-1x1"
+  },
+  {
+    src: "https://images.unsplash.com/photo-1584395630827-860eee694d7b?q=80&w=2400&auto=format&fit=crop",
+    alt: "Ilustración con temática natural y colores vivos",
+    span: "span-2x2"
+  }
+];
