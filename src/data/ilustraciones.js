f6b90405058f285a9923bd5e19378c7bdb4fe0b2
eskalona17 <fer.escalonso@gmail.com>
2025-06-29 12:14:01 +0200

    deploy

--- src/data/ilustraciones.js
+++ src/data/ilustraciones.js
@@ -1,28 +1,55 @@
-// src/data/ilustraciones.js
 export const ilustraciones = [
+
   {
-    src: "https://images.unsplash.com/vector-1749272151392-0189e836c426?q=80&w=2360&auto=format&fit=crop",
-    alt: "Ilustración abstracta con formas geométricas",
-    span: "span-2x2"
+    src: "/img/ilustraciones/ilustracion_4_web.png",
+    alt: "Ilustración con temática natural y colores vivos"
   },
   {
-    src: "https://plus.unsplash.com/premium_vector-1739921187869-0562a09fe3f9?q=80&w=2030&auto=format&fit=crop",
-    alt: "Ilustración vectorial de paisaje",
-    span: "span-1x1"
+    src: "/img/ilustraciones/ilustracion_2_web.jpg",
+    alt: "Ilustración con temática natural y colores vivos"
   },
   {
-    src: "https://images.unsplash.com/photo-1522199710521-72d69614c702?q=80&w=2400&auto=format&fit=crop",
-    alt: "Escena artística en tonos cálidos",
-    span: "span-3x1"
+    src: "/img/ilustraciones/amapola_web.jpg",
+    alt: "Ilustración abstracta con formas geométricas"
   },
   {
-    src: "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?q=80&w=2400&auto=format&fit=crop",
-    alt: "Ilustración de retrato digital",
-    span: "span-1x1"
+    src: "/img/ilustraciones/camiseta_patron_web.jpg",
+    alt: "Ilustración vectorial de paisaje"
   },
   {
-    src: "https://images.unsplash.com/photo-1584395630827-860eee694d7b?q=80&w=2400&auto=format&fit=crop",
-    alt: "Ilustración con temática natural y colores vivos",
-    span: "span-2x2"
-  }
+    src: "/img/ilustraciones/pelusilla_web.jpg",
+    alt: "Ilustración con temática natural y colores vivos"
+  },
+  {
+    src: "/img/ilustraciones/Cartel_mansabora_web.jpg",
+    alt: "Escena artística en tonos cálidos"
+  },
+  {
+    src: "/img/ilustraciones/patron_web.jpg",
+    alt: "Ilustración con temática natural y colores vivos"
+  },
+  {
+    src: "/img/ilustraciones/gorro.png",
+    alt: "Ilustración con temática natural y colores vivos"
+  },
+  {
+    src: "/img/ilustraciones/cartel_web.jpg",
+    alt: "Ilustración de retrato digital"
+  },
+  {
+    src: "/img/ilustraciones/marcapaginas_web.jpg",
+    alt: "Ilustración con temática natural y colores vivos"
+  },
+  {
+    src: "/img/ilustraciones/taza_web.jpg",
+    alt: "Ilustración con temática natural y colores vivos"
+  },
+  {
+    src: "/img/ilustraciones/ilustracion_3_web.jpg",
+    alt: "Ilustración con temática natural y colores vivos"
+  },
+  {
+    src: "/img/ilustraciones/ilustracion_web.jpg",
+    alt: "Ilustración con temática natural y colores vivos"
+  },
 ];
